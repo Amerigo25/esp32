e35d95c2dcb3b2ef9a2132365778903c146f96b8
Amerigo25 <93442930+Amerigo25@users.noreply.github.com>
2024-05-30 16:23:13 +0200

    final project

--- main/main.c
+++ main/main.c
@@ -7,12 +7,15 @@
 #include "freertos/task.h"
 #include "esp_check.h"
 #include "sdkconfig.h"
-#include "driver/i2s_std.h"
+
+#include <driver/i2s.h>
 #include "driver/i2c.h"
+#include "driver/gpio.h"
+
 #include "sh1107.h" /* sh1107 driver*/
 	
 #define I2S_WS	GPIO_NUM_25 /* word select line*/
-#define I2S_SD	GPIO_NUM_27 /* serial data line*/
+#define I2S_SD	GPIO_NUM_33 /* serial data line*/
 #define I2S_SCK	GPIO_NUM_32 /* bit clock line*/
 #define I2S_PORT I2S_NUM_0 /* i2s processor*/
 
@@ -22,11 +25,11 @@
 #define I2C_NUM I2C_NUM_0
 #define I2C_MASTER_FREQ_HZ 400000
 
-#define SAMPLE_RATE 8000
-#define BITS_PER_SAMPLE	16
-#define SAMPLE_SIZE	(BITS_PER_SAMPLE * 1024)
-#define BYTE_RATE	(SAMPLE_RATE * (BITS_PER_SAMPLE / 8))
+#define BUZZER 21
 
+#define SAMPLE_RATE 16000
+#define BUFFER_SIZE	256
+#define SCREEN_WIDTH 128
 
 #define WAVE_FREQ_HZ	100
 #define SAMPLE_PER_CYCLE (SAMPLE_RATE/WAVE_FREQ_HZ)
@@ -34,162 +37,201 @@
 #define AMPLITUDE 32   // Amplitude of the sine wave
 #define OFFSET 31.5 
 
+int16_t* r_buff;
+int16_t* draw_buff;
+volatile int displayIndex = 0;
+static SH1107_t dev; // oled handle
 
-static uint16_t r_buff[64]; 
-static i2s_chan_handle_t rx_handle = NULL;
-static i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2C_NUM_0, I2S_ROLE_MASTER);
-
-
-static i2s_std_config_t std_cfg = {
-        .clk_cfg  = I2S_STD_CLK_DEFAULT_CONFIG(SAMPLE_RATE),
-        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
-        .gpio_cfg = {
-            .mclk = I2S_GPIO_UNUSED,   
-            .bclk = I2S_SCK,
-            .ws   = I2S_WS,
-            .dout = I2S_GPIO_UNUSED,
-            .din  = I2S_SD,
-            .invert_flags = {
-                .mclk_inv = false,
-                .bclk_inv = false,
-                .ws_inv   = false,
-            },
-        },
-    };
+void init_i2s(void)
+{
+	i2s_config_t i2s_config = {
+    .mode = I2S_MODE_MASTER | I2S_MODE_RX,
+    .sample_rate = SAMPLE_RATE,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 4,
+    .dma_buf_len = BUFFER_SIZE,
+    .use_apll = false,
+    .tx_desc_auto_clear = false,
+    .fixed_mclk = 0};
 	
+	i2s_pin_config_t i2s_mic_pins = {
+    .bck_io_num = I2S_SCK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = I2S_PIN_NO_CHANGE,
+    .data_in_num = I2S_SD};
+	
+	ESP_ERROR_CHECK(i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL));
+    ESP_ERROR_CHECK(i2s_set_pin(I2S_NUM_0, &i2s_mic_pins));
+}
+
+void init_oled (void)
+{
+	i2c_master_init(&dev,I2C_SDA,I2C_SCL,0);
+	sh1107_init(&dev, 64, 128); /* width x height */
+	sh1107_contrast(&dev, 0xff);
+	sh1107_clear_screen(&dev, false);
+	sh1107_direction(&dev, DIRECTION90);
+
+}
+
 void generateSineWave(uint8_t* buffer, int bufferSize, float frequency) {
     for (int i = 0; i < bufferSize; i++) {
         float angle = (float)i / (bufferSize - 1) * 2 * PI * frequency; // Calculate angle in radians
         buffer[i] = (uint8_t)(AMPLITUDE * sin(angle) + OFFSET); // Generate sine wave values
     }
 }
-void generateCosWave(uint8_t* buffer, int bufferSize, float frequency) {
-    for (int i = 0; i < bufferSize; i++) {
-        float angle = (float)i / (bufferSize - 1) * 2 * PI * frequency; // Calculate angle in radians
-        buffer[i] = (uint8_t)(AMPLITUDE * cos(angle) + OFFSET); // Generate sine wave values
-    }
-}
-void scrollSineWave(uint8_t* buffer, int bufferSize, int scrollAmount) {
-    // Shift the entire buffer to the left by scrollAmount pixels
-    for (int i = 0; i < bufferSize - scrollAmount; i++) {
-        buffer[i] = buffer[i + scrollAmount];
-    }
-    
-    // Generate new values for the scrolled portion of the buffer
-    generateSineWave(buffer + (bufferSize - scrollAmount), scrollAmount, 1.0); // Adjust frequency as needed
-}
+
+/* 
+	draw one pixel at position (x,y) of the screen, x in range [0,127], y in range [0,63]
+	
+*/
 void sh1107_draw_pixel(SH1107_t* dev, int xpos, int ypos)
 {
 	int _seg = 63 - ypos;
 	int page = (xpos / 8);
 	int bit = (xpos % 8);
 	uint8_t mask = 1 << bit;
-	//wk0 = dev->_page[page]._segs[_seg];
-	//byte = sh1107_rotate_byte(byte);
 	dev->_page[page]._segs[_seg] |= mask;
 }
+
+/* 
+	draw one pixel at position (x,y) of the screen, x in range [0,127], y in range [0,63]
 	
-void sh1107_plot_audio(SH1107_t* dev, uint8_t* buffer)
+*/
+void sh1107_draw_line(SH1107_t* dev, int xpos, int ypos)
 {
-	for (int x=0; x<128; x++) {
-		int y = buffer [x]; // int between 0 and 63
+	int _seg = 63 - ypos;
+	int page = (xpos / 8);
+	int bit = (xpos % 8);
+	uint8_t mask = 1 << bit;
+	for (int i=0; i<_seg; i++) {
+		dev->_page[page]._segs[i] |= mask;
+	}
+}
+
+uint8_t map (int16_t x) {
+     return (uint8_t)((x+32768)*63/65534);
+}
+
+void sh1107_plot_audio(SH1107_t* dev, int16_t* buffer, int start)
+{
+	char x_char[8];
+	char y_char[8];
+	for (int x=0; x<SCREEN_WIDTH; x++) {
+		// x->[0,127], y->[0,63]
+		int i = (displayIndex+x)%SCREEN_WIDTH;
+		uint8_t y = map(buffer[i]); // map function [-32768,32767] -> [0,63]
+		sprintf(x_char,"x: %d",x%SCREEN_WIDTH);
+		sprintf(y_char,"y: %d",y-31);	
+		sh1107_display_text(dev,0,0,x_char,6,false);
+		sh1107_display_text(dev,1,0,y_char,6,false);
 		sh1107_draw_pixel(dev,x,y);
+		sh1107_show_buffer(dev);
+	}
+}
+
+/* read audio data task */
+void readTask(void *args) {
+	
+	size_t r_bytes = 0;
+	size_t r_samples = 0;	
+
+	while(1) {
+		// r_buff -> when passing an array to a function it decays to a pointer to is first element!					
+		if (i2s_read(I2S_NUM_0, r_buff, BUFFER_SIZE*sizeof(int16_t), &r_bytes, 1000) == ESP_OK) {
+			r_samples = r_bytes/sizeof(int16_t);
+			for (int i = 0; i < r_samples; i++) {
+				draw_buff[displayIndex] = r_buff[i];
+				displayIndex = (displayIndex+1)%SCREEN_WIDTH;
+			}
+		}
+	}
+}
+
+/* plot audio data task */
+void drawTask(void *args) {
+	
+	while(1) {
+		sh1107_clear_screen(&dev,false);
+		sh1107_plot_audio(&dev,draw_buff,0);
+		vTaskDelay(pdMS_TO_TICKS(20));		
+
 	}
 }
 
 
-void app_main(void)
-{
-		
-	/* initialize sh1107 */
-	SH1107_t dev;
-	i2c_master_init(&dev,I2C_SDA,I2C_SCL,0);
-	sh1107_init(&dev, 64, 128); /* width x height */
-	sh1107_contrast(&dev, 0xff);
-	sh1107_clear_screen(&dev, false);
 
-	uint8_t sin [128];
-	uint8_t cos [128];
+/*
+void plot_task (void *args){
 
-	uint8_t tri [128];
-
-	 
-
-	/* initialize i2s microphone 
-	ESP_ERROR_CHECK(i2s_channel_init_std_mode(rx_handle, &std_cfg));
-	ESP_ERROR_CHECK(i2s_channel_enable(rx_handle));
-	printf("fin qui, ci siamo arrivati\n"); 
-	*/
-
-
-	//init_microphone();
+	init_i2s();
+	init_oled(&dev);
+	int16_t* r_buff = (int16_t*)malloc(BUFFER_SIZE*sizeof(int16_t));
+	uint8_t* draw_buff = (uint8_t*)malloc(BUFFER_SIZE); // circular buffer 
 	
-	
-    while(1) {
-		
-		
-		/* setting screen*/
-		float f = 2;
-		generateSineWave (sin,128,f);
-		generateCosWave (cos,128,f);
+	size_t r_bytes = 0;
+	size_t r_samples = 0;			
+	sh1107_direction(&dev, DIRECTION90);
+	sh1107_display_text(&dev,0,0,"x: 0",6,false);
+	sh1107_display_text(&dev,1,0,"y: 0",6,false);
 
-		
-		/*
-		for (int i=0; i<128; i++) {
-			for (int j=63;j>0; j--) {
-				sh1107_draw_pixel(&dev,i,j);
-			}
-		}
-		*/
-
-		sh1107_plot_audio(&dev,sin);
-		sh1107_plot_audio(&dev,cos);
-		sh1107_show_buffer(&dev);
-		vTaskDelay(4);
-		
-		
-		//size_t r_bytes = 0;
-		/* r_buff -> when passing an array to a function it decays to a pointer to is first element!*/
-		/*
-		esp_err_t result = i2s_channel_read(rx_handle, r_buff, SAMPLE_SIZE, &r_bytes, 1000);
-		if (result == ESP_OK) {
-			float mean = 0;
-			r_samples = r_bytes/2;
-
-			for (int16_t i = 0; i < r_samples; i++) {
+	while(1) {
+		// r_buff -> when passing an array to a function it decays to a pointer to is first element!					
+		if (i2s_read(I2S_NUM_0, r_buff, BUFFER_SIZE*sizeof(int16_t), &r_bytes, 1000) == ESP_OK) {
+			r_samples = r_bytes/sizeof(int16_t);
+			printf("Number of samples: %d\n",r_samples);
+			
+			int16_t max = 0;
+			int16_t min = 0;
+			for (int i = 0; i < r_samples; i++) {
 				
-				mean += r_buff[i];
-				if (i%500 == 0) {
-				printf("%" PRId16 "\n",r_buff[i]);
+				if (r_buff[i] > max) {
+					max = r_buff[i];
 				}
-			}
+				if (r_buff[i] < min) {
+					min = r_buff[i];
+				}
 				
-			mean /= r_samples;
-			//printf("%.d\n",r_samples); // 8192 samples 16184 bytes
-			//printf("%.3f\n",mean);
-			// printf ("%d\n",r_samples); : 1024
+				printf("max: %d\n",max);
+				printf("min: %d\n",min);
+				
+				printf("%d\n",r_buff[i]);
+			}
 			
-			//vTaskDelay(1000 / portTICK_PERIOD_MS);
-			//sh1107_plot_audio (&dev, r_buff, BUFFER_SIZE);
-			//sh1107_display_text(&dev, 0, 0, "Reading from microphone...", 16, false);
-		}
-		
 
-		else {
-            sh1107_display_text(&dev, 0, 0, "Read failed!", 13, false);
-		}
-		
-		*/
+			buffer_to_draw(r_buff,draw_buff);
+			sh1107_plot_audio(&dev,draw_buff,0);
+	
 
-	         		
+
+		}
+	}
+		free(r_buff);
+		free(draw_buff);
+		vTaskDelete(NULL);
+}
+*/
+
+void app_main(void)
+{			
+		init_i2s();
+		init_oled();
 		
-			/*
-			vTaskDelay(2000 / portTICK_PERIOD_MS);
-			sh1107_fadeout(&dev);
-			fflush(stdout);
+		r_buff = (int16_t *)malloc(BUFFER_SIZE*sizeof(int16_t));
+		draw_buff = (int16_t *)malloc(128*sizeof(int16_t)); // circular buffer 
+
+		xTaskCreatePinnedToCore(&readTask,"readTask",2048,NULL,1,NULL,0);
+		xTaskCreatePinnedToCore(&drawTask,"drawTask",2048,NULL,1,NULL,1);
+
+		
+		//sh1107_fadeout(&dev);
+		//fflush(stdout);
 			
-			printf("Restarting now.\n");
-			esp_restart();
-			*/
-	}		
+		//printf("Restarting now.\n");
+		//esp_restart();
+		
+	
 }
